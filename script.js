15a74172d6a91106ba41e475c24a64b11b5ce313
Panta4aa <165237628+BlackScythe99@users.noreply.github.com>
2026-04-12 12:59:29 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function openLightbox(img) {
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if (position < screenHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
